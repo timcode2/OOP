e60f41714389b42a58c62d5ffcd400ae59fc8859
artemm <artem.m2013@gmail.com>
2023-06-12 18:38:53 +0300

    get 100% tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,31 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+
+def test_calculate_total_price():
+    item1 = Item("Смартфон", 10000, 20)
+    item2 = Item("Ноутбук", 20000, 5)
+
+    assert item1.calculate_total_price() == 200000
+    assert item2.calculate_total_price() == 100000
+
+
+def test_apply_discount():
+    item1 = Item("Смартфон", 10000, 20)
+    item2 = Item("Ноутбук", 20000, 5)
+
+    Item.pay_rate = 0.8
+
+    item1.apply_discount()
+    item2.apply_discount()
+
+    assert item1.price == 8000.0
+    assert item2.price == 16000.0
+
+
+def test_instances():
+    item1 = Item("Смартфон", 10000, 20)
+    item2 = Item("Ноутбук", 20000, 5)
+
+    assert item1 in Item.all
+    assert item2 in Item.all
